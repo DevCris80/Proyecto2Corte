bfa1f4eb733620ae4f54e251229e1522df8daa25
CrisPlays80 <cristian.steven05@gmail.com>
2026-04-26 16:35:34 -0500

    Conectar todas las rutas en main

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 
+from app.routes.proveedores_routes import router as proveedor_router
+from app.routes.productos_routes import router as productos_router
+from app.routes.ventas_routes import router as ventas_router
+from app.routes.optimizacion_routes import router as optimizacion_router
+
 app = FastAPI()
+
+app.include_router(router=proveedor_router)
+app.include_router(router=productos_router)
+app.include_router(router=ventas_router)
+app.include_router(router=optimizacion_router)
 
 @app.get("/")
 async def raiz():
-    return {"Saludo": "Hola profe como estas?"}
+    return {"Saludo": "Hola profe"}
