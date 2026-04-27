64fc2d365c9817f8faa38e1ca2eef4642507dc96
CrisPlays80 <cristian.steven05@gmail.com>
2026-04-27 16:39:39 -0500

    Cambiar response model a propio

--- app/repository/csv_orm.py
+++ app/repository/csv_orm.py
@@ -55,7 +55,7 @@ def eliminar_csv(id_registro: str, ruta_archivo: str):
 
     return True
 
-def actualizar_csv(objeto_id: str, campo_id: str, nuevos_datos: Any, ruta_archivo: str):
+def actualizar_csv(objeto_id: str, campo_id: str, nuevos_datos: Type[BaseModel], ruta_archivo: str):
     if not os.path.isfile(ruta_archivo):
         return False
 
